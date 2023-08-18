a650c3ed4c73011341d76968c61bd5f647c9f7ac
周锐涛 <3508214035@qq.com>
2023-08-18 19:10:45 +0800

    完成了购物车的开发

--- main.js
+++ main.js
@@ -12,6 +12,18 @@ uni.$http = $http;
 // 配置请求根路径
 $http.baseUrl = 'https://api-hmugo-web.itheima.net';
 
+// 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
 // 封装的展示消息提示的方法
 uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
   uni.showToast({
@@ -24,10 +36,17 @@ uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
 // 取消vue开发工具的警告
 Vue.config.productionTip = false
 
+// 在 uni-app 中，每个页面或组件都有一个 mpType 属性，
+// 用于指定文件的类型，可以是 'app'、'page' 或 'component'。
 App.mpType = 'app'
 
+// 引入store对象
+import store from './store/store'
+
 const app = new Vue({
-  ...App
+  ...App,
+  // 将 store 挂载到 Vue 实例上
+  store
 })
 app.$mount()
 // #endif
